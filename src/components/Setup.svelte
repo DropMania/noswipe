bb55bfada0070c0255bfd7491ac13c3a4b659e57
Robin <robinhallerbach13@googlemail.com>
2021-08-18 20:38:58 +0200

    update setup

--- src/components/Setup.svelte
+++ src/components/Setup.svelte
@@ -1,14 +1,24 @@
 <script>
-    import { Tabs, Tab, TextField, Button, ProgressCircular } from 'smelte'
+    import { TextField, Button, ProgressCircular, DatePicker } from 'smelte'
     import supabase from '../supabase'
     import { v4 as uuidv4 } from 'uuid'
+    import { crossfade } from 'svelte/transition'
 
+    let state = 'birthday'
+    let formData = {
+        name: '',
+        birthday: '',
+        gender: '',
+        searchfor: '',
+        minage: 18,
+        maxage: 99,
+        distance: null
+    }
     let noImgYet = false
     let uploading = false
     let currentImgPath = ''
-    //set a default profile image
-
     let image = 'assets/blank.jpg'
+    export let selected = '10.12.2020'
     let checkImage = async () => {
         uploading = true
         let { data } = await supabase
@@ -78,57 +88,55 @@
     }
 </script>
 
-<Tabs
-    selected="1"
-    loading={false}
-    class="bg-black shadow-sm "
-    classes="absolute bottom-0 w-full border-t-2 border-gray-800"
-    tabButtonClasses="text-white"
-    color="primary"
-    let:selected
-    items={[
-        { id: '1', text: 'Name', icon: 'person' },
-        { id: '2', text: 'Birthday', icon: 'today' },
-        { id: '3', text: 'Looking for', icon: 'face' }
-    ]}
->
-    <div
-        slot="content"
-        class="flex items-center content-center overflow-hidden w-full  h-full"
-    >
-        <Tab id="1" {selected}>
-            <div
-                class="flex flex-col gap-y-10 h-full w-full justify-center items-center mt-10"
-            >
-                <div class="relative">
-                    <!-- svelte-ignore a11y-img-redundant-alt -->
-                    <img
-                        src={image}
-                        alt="profile image"
-                        class="border-2 border-gray-200 mr-3 rounded-full w-60"
-                        use:onload
+<div class="flex items-center content-center overflow-hidden w-full h-full">
+    {#if state == 'name'}
+        <div
+            class="flex flex-col gap-y-10 h-full w-full justify-center items-center mt-10"
+            transition:crossfade
+        >
+            <div class="relative">
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <img
+                    src={image}
+                    alt="profile image"
+                    class="border-2 border-gray-200 mr-3 rounded-full w-60"
+                    use:onload
+                />
+                {#if !noImgYet}
+                    <Button
+                        icon="edit"
+                        classes="absolute top-44 left-44"
+                        on:click={updateImg}
                     />
-                    {#if !noImgYet}
-                        <Button
-                            icon="edit"
-                            classes="absolute top-44 left-44"
-                            on:click={updateImg}
-                        />
-                    {/if}
-                    {#if uploading}
-                        <div class="absolute top-20 left-20">
-                            <ProgressCircular />
-                        </div>
-                    {/if}
-                </div>
-
-                {#if noImgYet}
-                    <Button on:click={uploadFile}>Upload Profile Image</Button>
                 {/if}
-                <TextField label="Enter your name" />
+                {#if uploading}
+                    <div class="absolute top-24 left-24">
+                        <ProgressCircular />
+                    </div>
+                {/if}
             </div>
-        </Tab>
-        <Tab id="2" {selected}>Birthday</Tab>
-        <Tab id="3" {selected}>Looking for</Tab>
-    </div>
-</Tabs>
+
+            {#if noImgYet}
+                <Button on:click={uploadFile}>Upload Profile Image</Button>
+            {/if}
+            <TextField label="Enter your name" bind:value={formData.name} />
+            <Button
+                disabled={formData.name == ''}
+                icon="keyboard_arrow_right"
+                on:click={() => (state = 'birthday')}>Go on</Button
+            >
+        </div>
+    {:else if state == 'birthday'}
+        <div
+            transition:crossfade
+            class="flex flex-col gap-y-10 h-full w-full justify-center items-center mt-10"
+        >
+            <DatePicker open bind:value={selected} />
+            <Button
+                disabled={formData.name == ''}
+                icon="keyboard_arrow_right"
+                on:click={() => (state = 'birthday')}>Go on</Button
+            >
+        </div>
+    {/if}
+</div>
