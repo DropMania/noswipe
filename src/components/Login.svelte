2915ec88f814c9ec36a900bf167aa19952383b06
Robin <robinhallerbach13@googlemail.com>
2021-08-31 19:52:34 +0200

    fixed login

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,23 +1,10 @@
 <script>
-    import { Device } from '@capacitor/device'
-    import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth'
-    import { auth } from '../firebase'
+    import { cfaSignIn } from 'capacitor-firebase-auth'
     import { Button } from 'smelte'
 
-    let loginGoogle = async () => {}
-    Device.getInfo().then((info) => {
-        if (info.platform === 'web') {
-            const provider = new GoogleAuthProvider()
-            loginGoogle = () => {
-                signInWithRedirect(auth, provider)
-            }
-        } else {
-            const { cfaSignIn } = import('capacitor-firebase-auth')
-            loginGoogle = async () => {
-                cfaSignIn('google.com').subscribe(async (user) => {})
-            }
-        }
-    })
+    let loginGoogle = async () => {
+        cfaSignIn('google.com').subscribe(async (user) => {})
+    }
 </script>
 
 <Button on:click={loginGoogle}>Sign In with Google</Button>
